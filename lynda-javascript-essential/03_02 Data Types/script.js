435e7f2725f8ce9c6300fcf8ecbbf47f4cd470c0
Li Li <levonlee@gmail.com>
2020-04-06 15:05:56 -0400

    array

---
+++ lynda-javascript-essential/03_02 Data Types/script.js
@@ -0,0 +1,11 @@
+var negInteger = -3.14159265359; // numeric
+var escQuote = "Quotes can also be \"escaped\"."; // string
+var theSunIsWarm = true; // boolean
+var emptyInside = null; // null
+var justAnotherVariable;
+
+// undefined
+// Symbol
+
+// Try this in your console:
+// console.log(typeof insertVariableName);
