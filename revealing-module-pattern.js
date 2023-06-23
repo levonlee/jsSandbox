5aae213ed74548e271217c80306274b86ace2c18
Li Li <5024766+levonlee@users.noreply.github.com>
2023-06-23 10:23:21 -0400

    added more to revealing module pattern

--- revealing-module-pattern.js
+++ revealing-module-pattern.js
@@ -23,14 +23,23 @@ Singleton = function () {
         getValue: function () {
             return privateCounter;
         },
-        callbackExample: function () {
+        callbackExample_MakePrivateMethodAvailableToCallbackInsidePublicMethod: function () {
             var localGetValue = this.getValue;
             [].forEach.call(
                 document.getElementsByClassName('my-class'),
                 function (ele) {
                     changeBy(1);
-                    console.log('callBackExample', localGetValue());
+                    console.log(
+                        'callbackExample_MakePrivateMethodAvailableToCallbackInsidePublicMethod',
+                        localGetValue()
+                    );
                 }
+            )
+        },
+        callbackExample_CallbackFromOutside: function (cbExternal) {
+            [].forEach.call(
+                document.getElementsByClassName('my-class'),
+                cbExternal
             )
         },
     };
@@ -40,15 +49,29 @@ Singleton = function () {
 Singleton.sayHello();
 
 Singleton.increment();
+
 // 1
 console.log(Singleton.getValue());
 
-Singleton.callbackExample();
-// 4
+// callbackExample_MakePrivateMethodAvailableToCallbackInsidePublicMethod 2
+// callbackExample_MakePrivateMethodAvailableToCallbackInsidePublicMethod 3
+// callbackExample_MakePrivateMethodAvailableToCallbackInsidePublicMethod 4
+Singleton.callbackExample_MakePrivateMethodAvailableToCallbackInsidePublicMethod();
+
+// callbackExample_CallbackFromOutside 5
+// callbackExample_CallbackFromOutside 6
+// callbackExample_CallbackFromOutside 7
+Singleton.callbackExample_CallbackFromOutside(function (ele) {
+    Singleton.increment(1);
+    console.log('callbackExample_CallbackFromOutside', Singleton.getValue());
+});
+
+// 7
 console.log(Singleton.getValue());
 
 Singleton.callBothPrivatePublicMethods();
-// 6
+
+// 9
 console.log(Singleton.getValue());
 
 // Non-singleton
@@ -65,6 +88,7 @@ Module = function () {
 };
 
 var moduleInstance1 = Module();
+moduleInstance1.sayHello();
 // Or
 var moduleInstance2 = new Module();
-moduleInstance1.sayHello();
+moduleInstance2.sayHello();
