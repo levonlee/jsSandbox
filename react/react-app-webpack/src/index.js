41b68fc1ccf3d2e069f1044e4f7da0a2c9147309
Li Li <levonlee@gmail.com>
2023-10-11 13:33:05 -0400

    react expose render function

--- react/react-app-webpack/src/index.js
+++ react/react-app-webpack/src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 
 // import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App library="My React" />
-//   </React.StrictMode>
-// );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
+function renderRoot() {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+        <React.StrictMode>
+            <App library="My React" />
+        </React.StrictMode>
+    );
+}
 
-export {React, ReactDOM, App}
-
+export {React, ReactDOM, renderRoot}
